386e62862c9958da609b0097736837132f90a4d7
ynunokawa <yusuke_nunokawa@esrij.com>
2015-11-02 19:29:53 +0900

    package.js for hosting demo

--- examples/config/package.js
+++ examples/config/package.js
@@ -8,18 +8,18 @@
     parseOnLoad: true,
     packages: [{
       name: 'react',
-      location: dir_path + '/bower_components/react',
-      //location: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/',
+      //location: dir_path + '/bower_components/react',
+      location: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0',
       main: 'react'
     }, {
       name: 'react-dom',
-      location: dir_path + '/bower_components/react',
-      //location: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/',
+      //location: dir_path + '/bower_components/react',
+      location: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0',
       main: 'react-dom'
     }, {
       name: 'fixedDataTable',
-      location: dir_path + '/bower_components/fixed-data-table/dist',
-      //location: 'https://cdnjs.cloudflare.com/ajax/libs/fixed-data-table/0.4.7/',
+      //location: dir_path + '/bower_components/fixed-data-table/dist',
+      location: 'https://cdnjs.cloudflare.com/ajax/libs/fixed-data-table/0.4.7',
       main: 'fixed-data-table'
     }, {
       name: 'reactWidgets',
